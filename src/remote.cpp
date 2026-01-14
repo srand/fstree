ff1e7c8be3d3941bac7daa3ef79afe326750aed9
Robert Andersson <rbrt.andersson@outlook.com>
2026-01-15 00:35:09 +0100

    fix nullptr deref if remote url is invalid
    Signed-off-by: Robert Andersson <rbrt.andersson@outlook.com>

--- src/remote.cpp
+++ src/remote.cpp
@@ -10,7 +10,7 @@ std::unique_ptr<remote> remote::create(const url& address) {
   if (address.scheme() == "jolt" || address.scheme() == "tcp") {
     return std::make_unique<remote_jolt>(address);
   }
-  return nullptr;
+  throw std::invalid_argument("unsupported remote scheme: " + address.scheme());
 }
 
 }  // namespace fstree
