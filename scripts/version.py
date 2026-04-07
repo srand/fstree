d7f7f4b4816b1ec559563fd9bbad4ef7212882dd
Robert Andersson <robert.m.andersson@atlascopco.com>
2026-04-07 15:27:29 +0200

    github: update wheel version number on build
    Signed-off-by: Robert Andersson <robert.m.andersson@atlascopco.com>

--- scripts/version.py
+++ scripts/version.py
@@ -5,3 +5,9 @@ version = time.strftime("%y.%m.") + sys.argv[1]
 
 with open("src/version.hpp", "w") as f:
     f.write(f'#define FSTREE_VERSION "{version}"')
+
+# Update pyproject.toml with the new version number
+with open("pyproject.toml", "r") as f:
+    pyproject =  f.read()
+with open("pyproject.toml", "w") as f:
+    f.write(pyproject.replace('version = "0.1.0"', f'version = "{version}"'))
